fd1a8f4b30cb6e2e04cb1730c8ae0d4f16387266
sweettedcafe <baitajay25@gmail.com>
2026-06-06 15:28:38 +0800

    Update vite config and lock file

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@tanstack/react-start/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react(),
-    tailwindcss(),
-    tsconfigPaths(),
-  ],
-  server: {
-    port: 3000,
+  tsr: {
+    appDirectory: "src",
   },
-})
+  vite: {
+    plugins: [tsconfigPaths()],
+  },
+});
